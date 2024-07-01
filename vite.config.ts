7b8ce2988957963c31f47c6d7ade860ac03db659
Hailu03 <10421016@student.vgu.edu.vn>
2024-07-01 15:29:52 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
 
-  server: {
-    port: 3000,
+//   server: {
+//     port: 3000,
+//   }
+// })
+
+export default defineConfig({
+  base: 'AdminLibrary',
+  build: {
+    outDir: 'dist'
   }
-})
+});
